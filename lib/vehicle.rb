37bce2af97fc2229c4947a89426eb0ac3c6f67bd
Jobsidney <jobsidney67@gmail.com>
2022-08-20 16:51:43 +0300

    Done with assignment

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,27 @@
 class Vehicle
 
+    attr_accessor :wheel_size, :wheel_number
+
+    def initialize(wheel_size,wheel_number)
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
+
+    def go
+        "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank
+        "filling up!"
+    end
 end
+
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+end
+
+car=Car.new(23,4)
+p car.go
